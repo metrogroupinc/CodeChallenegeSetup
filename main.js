69d026b7cf12bdd4ca8b79225140d341f24a5cda
siluski <47801828+siluski@users.noreply.github.com>
2021-02-02 13:58:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Global currentCatTax variable
 let currentCatTax = 0;
 
+
 // TODO: calcButtonClick function
 // Function should handle the following items:
 // 1) Generate a random, whole number between 0 and 20.
@@ -11,7 +12,40 @@ let currentCatTax = 0;
 // 6) If the random number is 0, update the pay button so that it is hidden.
 // 7) Both the amountOwed and pay amount button should be updated every time the calculate cat tax button is clicked.
 
-function calcButtonClick() {}
+ let buttonList = document.getElementsByClassName("calculate btn-primary");
+ let calcButton = buttonList[0];
+
+ buttonList = document.getElementsByClassName("payBtn btn-success");
+ let payBtn = buttonList[0];
+ payBtn.style.display = "none";
+
+let imageContainer = document.getElementsByClassName("imageContainer")[0];
+
+ calcButton.addEventListener("click", calcButtonClick);
+payBtn.addEventListener("click", payButton);
+
+let message = document.getElementById("amountOwed");
+
+
+function calcButtonClick(e) {
+
+    currentCatTax = Math.random() * 21;
+    currentCatTax = Math.round(currentCatTax);
+
+    if(currentCatTax > 0)
+    {
+        message.innerText = `You owe ${currentCatTax} cat tax! Pay up!`;
+        payBtn.style.display = "block";
+    }
+    else
+    {
+        message.innerText = `You owe 0 cat tax! You've escaped this time!`;
+        payBtn.style.display = "none";
+    }
+
+
+
+}
 
 // TODO: payButton function
 // Function should handle the following items:
@@ -22,4 +56,36 @@ function calcButtonClick() {}
 // 5) Once the cat image is retrieved, append the image to the image container
 // 6) If the cat wax was not payable (amount was less than or equal to 0) when the button was clicked, replace the entire contents of the container with the gif found here (https://gfycat.com/snivelingbeautifuljoey-cat)
 
-function payButton() {}
+function payButton(e) {
+    currentCatTax--;
+    if(currentCatTax > 0)
+    {
+        message.innerText = `You still owe ${currentCatTax} cat tax! Pay up!`;
+
+        fetch("https://api.thecatapi.com/v1/images/search")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                let img = document.createElement("img");
+                img.src = data[0].url;
+                imageContainer.appendChild(img);
+            
+            })
+            .catch(error => console.error(error))
+
+    }
+    else if(currentCatTax == 0){
+        message.innerText = `Your debts are paid...`;
+    }
+    else{
+        e.preventDefault();
+        imageContainer.innerHTML = '';
+        let video = document.createElement("video");
+        video.src = "https://thumbs.gfycat.com/SnivelingBeautifulJoey-mobile.mp4";
+        video.autoplay = true;
+        video.loop = true;
+        imageContainer.appendChild(video);
+
+    }
+
+}
