82aa7c78bbe8f769bdeac8d4f832f9634912c683
Darren Zeng <darren.zeng@stonybrook.edu>
2021-02-22 21:08:05 -0500

    Implement calculate and pay buttons

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 //Global currentCatTax variable
 let currentCatTax = 0;
 
+const calculateButton = document.getElementById("calculateBtn");
+console.log(calculateButton);
+calculateButton.addEventListener("click", calcButtonClick);
+const payBtn = document.getElementsByClassName("payBtn")[0];
+console.log(payBtn);
+payBtn.addEventListener("click", payButton);
+
 // TODO: calcButtonClick function
 // Function should handle the following items:
 // 1) Generate a random, whole number between 0 and 20.
@@ -11,7 +18,23 @@ let currentCatTax = 0;
 // 6) If the random number is 0, update the pay button so that it is hidden.
 // 7) Both the amountOwed and pay amount button should be updated every time the calculate cat tax button is clicked.
 
-function calcButtonClick() {}
+function calcButtonClick() {
+    let imageContainer = document.getElementsByClassName("imageContainer")[0];
+    imageContainer.innerHTML = '';
+    currentCatTax = Math.floor(Math.random() * 21);
+    console.log(currentCatTax);
+    const amountOwed = document.getElementById('amountOwed');
+    const payButton = document.getElementsByClassName('payBtn')[0];
+    if (currentCatTax != 0) {
+        amountOwed.innerHTML = "You owe " + currentCatTax + " cat tax! Pay up!";
+        payButton.innerHTML = "Pay Cat Tax";
+        payButton.style.visibility = "visible";
+    }
+    else {
+        amountOwed.innerHTML = "You owe " + currentCatTax + " cat tax! You've escaped this time!";
+        payButton.style.visibility = "hidden";
+    }
+}
 
 // TODO: payButton function
 // Function should handle the following items:
@@ -22,4 +45,40 @@ function calcButtonClick() {}
 // 5) Once the cat image is retrieved, append the image to the image container
 // 6) If the cat wax was not payable (amount was less than or equal to 0) when the button was clicked, replace the entire contents of the container with the gif found here (https://gfycat.com/snivelingbeautifuljoey-cat)
 
-function payButton() {}
+function payButton() {
+    console.log(currentCatTax);
+    prevCatTax = currentCatTax;
+    currentCatTax--;
+    const amountOwed = document.getElementById('amountOwed');
+    const payBtn = document.getElementsByClassName('payBtn')[0];
+    if (currentCatTax > 0) {
+        amountOwed.innerHTML = "You still owe " + currentCatTax + " cat tax! Pay up!";
+    }
+    else {
+        amountOwed.innerHTML = "Your debts are paid...";
+    }
+
+    if (prevCatTax > 0) {
+        fetch("https://api.thecatapi.com/v1/images/search", {
+            headers: { "Content-Type": "image" },
+            method: 'GET',
+        })
+            .then(response => (response.text()))
+            .then(result => {
+                console.log('Success:', result);
+                const imageContainer = document.getElementsByClassName("imageContainer")[0];
+                const image = document.createElement('img');
+                image.setAttribute('src', JSON.parse(result)[0].url);
+                image.setAttribute("height", "100px");
+                image.setAttribute("width", "125x");
+                imageContainer.prepend("       ", image);
+                console.log(JSON.parse(result)[0].url);
+                console.log(typeof (result));
+            }).catch(error => { console.error('Error:', error); });
+    }
+    else {
+        let imageContainer = document.getElementsByClassName("imageContainer")[0];
+        imageContainer.innerHTML = "<iframe src='https://gfycat.com/ifr/SnivelingBeautifulJoey' frameborder='0' scrolling='no' allowfullscreen width='640' height='846'></iframe>";
+    }
+}
+
